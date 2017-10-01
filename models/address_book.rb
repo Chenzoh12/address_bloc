0dc3704d78c5b3e75cdb472a6a647d2803e712b3
Vincent Scotti <scottivincentj@gmail.com>
2017-10-01 18:46:20 -0400

    End of checkpoint 6. CSV rspec.

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,12 +1,13 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
     attr_reader :entries
-    
+
     def initialize
         @entries = []
     end
- 
+
     def add_entry(name, phone_number, email)
         # #9
         index = 0
@@ -20,7 +21,18 @@ class AddressBook
         # #11
         entries.insert(index, Entry.new(name, phone_number, email))
     end
-    
+
+    def import_from_csv(file_name)
+    # Implementation goes here
+        csv_text = File.read(file_name)
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+        # #8
+        csv.each do |row|
+            row_hash = row.to_hash
+            add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+        end
+    end
+
     def remove_entry(name, phone_number, email)
         index = 0
         entries.each do |entry|
